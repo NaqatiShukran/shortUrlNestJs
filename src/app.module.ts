7ea1d17175acadb205ac5314c58d485b12590cb0
Shukran <shukkss@gmail.com>
2021-04-15 15:03:03 +0530

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UrlModule } from './shortUrl/shortUrl.module';
+
+@Module({
+  imports: [UrlModule, MongooseModule.forRoot('mongodb+srv://dbUser:Password@urlshortner.pjx1v.mongodb.net/urlShortner?retryWrites=true&w=majority')],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
