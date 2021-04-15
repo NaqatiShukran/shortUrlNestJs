16403689440e7433c106b3beb6a5378a9fa5bf19
Shukran <shukkss@gmail.com>
2021-04-15 15:17:44 +0530

    Comment added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UrlModule } from './shortUrl/shortUrl.module';
 
+//Module
 @Module({
   imports: [UrlModule, MongooseModule.forRoot('mongodb+srv://dbUser:Password@urlshortner.pjx1v.mongodb.net/urlShortner?retryWrites=true&w=majority')],
   controllers: [AppController],
