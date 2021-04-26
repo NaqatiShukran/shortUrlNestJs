3c391d03055fea5d45dea6b9d5a283b29bc27490
Shukran Naqati <shukkss@gmail.com>
2021-04-26 10:38:16 +0530

    Insert URL added for gQL

--- src/msUrl/msUrl.controller.ts
+++ src/msUrl/msUrl.controller.ts
@@ -10,26 +10,37 @@ interface IUrlObject{
     urlOb: Object
 }
 
+interface IOriginalUrl{
+    originalUrl : string
+}
+
 @Controller('url')
 export class UrlController {
     constructor(private readonly UrlService: UrlService) {}
 
     private logger = new Logger('AppController');
 
-    // @MessagePattern('url')
     @GrpcMethod('UrlController','GetShortUrl')
-    // async getShortUrl(urlHash: string)
     async getShortUrl(hash: IUrlHash, metadata: any): Promise<IUrlObject> {
         try{
-            // console.log("MS Controller getShortUrl");
-            // console.log("This is url in ms" + Url);
-            const Url= await this.UrlService.getShortUrl(hash.urlHash) 
-            // console.log("This is url in ms" + Url);        
-            return {urlOb:Url};
+            const Url= await this.UrlService.getShortUrl(hash.urlHash)        
+            return {urlOb: Url};
         }
         catch(error){
         }
         
+    }
+
+    @GrpcMethod('UrlController','InsertUrl') 
+    async insertUrl (originalUrl: IOriginalUrl, metadata: any): Promise<IUrlObject> {
+        try{
+            const Url= await this.UrlService.insertUrl(originalUrl.originalUrl)
+            console.log("controller ", Url);
+                    
+            return {urlOb: Url};
+        }
+        catch(error){
+        }
     }
 
 }
--- src/msUrl/msUrl.service.ts
+++ src/msUrl/msUrl.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException, Inject, Catch } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from 'mongoose';
+import * as ShortId from 'short-uuid'
 
 import { Url } from './msUrl.model';
 
@@ -15,19 +16,52 @@ export class UrlService{
         const Url = await this.findUrl(urlHash);
         if (Url==null){
             console.log("MS exceptiion error");
-            //throw new NotFoundException("Url not found");
         }
-        // console.log('Ms Service short url');
         
         return Url;
+    }
+
+    async insertUrl(originalUrl: string){
+        let findUrls = await this.findUrlByName(originalUrl);
+        console.log(findUrls);
+        
+        const BaseUrl = "localhost:4000/";
+        const urlHash = ShortId.generate();
+        const shortUrl = BaseUrl + urlHash;
+        console.log(urlHash);
+        
+        if (findUrls === null){
+            const newUrl = new this.urlModel({
+                url: originalUrl,
+                urlHash: urlHash,
+                shortUrl: shortUrl
+            });
+            
+            const result = await newUrl.save();
+            console.log(result);
+            return result;
+        }
+        else{
+            throw new Error ("Url already exists")
+        }
     }
 
     private async findUrl(urlHash: string): Promise<Url>{
         let data;
         try{
             data = await this.urlModel.findOne({urlHash: urlHash})
-            // console.log(data);
-            
+        }
+        catch(error){
+            throw new NotFoundException('Could not find url');
+        }
+        return data;
+    }
+
+    private async findUrlByName(url: string): Promise<Url>{
+        let data;
+        try{
+            data = await this.urlModel.findOne({url: url})
+            console.log(data);     
         }
         catch(error){
             throw new NotFoundException('Could not find url');
